6b665d76ac9033d3a0ab29ac50c20929313ee858
Chuan Tan <ct538@cam.ac.uk>
2018-07-16 18:06:32 +0100

    Remove insert_into_country and country table related functions

--- json_reader.py
+++ json_reader.py
@@ -2,6 +2,7 @@
 
 # json_reader.py
 # json_reader, by Chuan Tan <ct538@cam.ac.uk>
+#
 # Copyright (C) Chuan Tan 2018
 #
 # This programme is free software; you may redistribute and/or modify
@@ -13,44 +14,15 @@
 
 import psycopg2
 import json
-import pycountry
 import uuid
 
-def fix_amiacountry(country):
-    if country == "palestine":
-        return "PS"
-    if country == "kosovo":
-        return "XK"
-    if country == "macedonia":
-        return "MK"
-    if country == "laos":
-        return "LA"
-
-
-
-def add_uuid_and_country_code(data):
+def append_uuid(data):
     for JSONObject in data:
         institution_name = JSONObject["Institution"]
         JSONObject["Institution-uuid"] = str(uuid.uuid3(uuid.NAMESPACE_DNS,institution_name.encode("utf-8")))
         
-        country = JSONObject["Country"].lower()
-        # Fix for palestine, kosovo, macedonia and laos - both partially recognised states
-        if country in ["palestine","kosovo","macedonia","laos"]:
-            JSONObject["Country-code"] = fix_amiacountry(country)
-        else:
-            JSONObject["Country-code"] = pycountry.countries.lookup(country).alpha_2
-
     return data
         
-
-def insert_into_country(data):
-    
-    processed_data = set([ ( str(data[x]["Country"]) , str(data[x]["Country-code"]) ) for x in range(len(data)) ])
-    query = """INSERT INTO country (country_name,country_code)
-                VALUES ( %s , %s ); """
-    cursor.executemany(query,processed_data)
-    conn.commit()
-
 conn = psycopg2.connect(database='obp_institutions',
                         user = 'obp',
                         password = 'some_secret_password',
@@ -61,7 +33,9 @@ cursor = conn.cursor()
 with open("data.json") as f:
     print "Loading JSON file."
     data = json.load(f)
-    data = add_uuid_and_country_code(data)
+    data = append_uuid(data)
+
+    
 
 
 
